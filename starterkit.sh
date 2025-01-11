5026bce0e55a76d25c4eccc23cf819f88952356f
29-29 <ackx29sodo@gmail.com>
2025-01-12 01:09:09 +0800

    added installation of git cli in starter kit script

--- starterkit.sh
+++ starterkit.sh
@@ -6,10 +6,21 @@ sudo apt install git
 git config --global user.name "29-29"
 git config --global user.email "ackx29sodo@gmail.com"
 
+## credentials stuff
 sudo apt install pass
 git config --global credential.credentialStore gpg
 pass init 4EFAF11709ABA0875A5C9AE951B3B77D19B8B6B9
 git credential-manager github login
+
+## github cli
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
 
 ## lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
